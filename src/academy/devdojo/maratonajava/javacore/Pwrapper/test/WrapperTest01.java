a281f625ebadc55e89d388f50af172f8583dcb07
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-15 16:01:19 -0300

    estudando classes wrappers

---
+++ src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.Pwrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100D;
+        char charP = 'W';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;  // auto boxing
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        int i = intW;  // unboxing
+        Integer intW2 = Integer.parseInt("2");
+        System.out.println(intW2.getClass());
+    }
+}
