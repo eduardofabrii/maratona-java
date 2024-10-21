0f276364421040ea0cba50b59f79fb177499fae2
Eduardo Fabri <eduardohfabri@gmail.com>
2024-10-20 21:04:25 -0300

    Teste de StringBuilder

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "Willian Suane";
+        nome.concat(" DevDojo");
+        System.out.println(nome);
+
+        StringBuilder sb = new StringBuilder("Willian Suane");
+        sb.append(" DevDojo").append(" Academy");
+        System.out.println(sb);
+    }
+}
